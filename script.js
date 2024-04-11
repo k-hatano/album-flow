27d2d0560bdc1850f516cc087857151227d3ef84
k-hatano <kent.ruffle.mgj626@gmail.com>
2024-04-11 11:31:58 +0900

    タイトル表示、ストアフロントを変えられるように

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 
-const TOPSONGS_URL_HTTPS = "https://itunes.apple.com/us/rss/topsongs/country=jp/cc=jp/l=jp/limit=50/xml";
-const TOPSONGS_URL = "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topsongs/country=jp/cc=jp/l=jp/limit=50/xml";
+const TOPSONGS_URL_HTTPS = "https://itunes.apple.com/us/rss/topsongs/country=us/cc=us/l=us/limit=50/xml";
+const TOPSONGS_URL = "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topsongs/country=us/cc=us/l=us/limit=50/xml";
+
+const STOREFRONT_MAPPING_PATH = "2208578ba151d1d7c4edeeda15b4e9b1/storefrontmappings.json";
 
 // 143462
 
 let SF = undefined;
+let L = undefined;
 
 onload = _ => {
   initialize();
-  loadTopSongs(result => loadedTopSongs(result));
+  loadTopSongs(result => {
+    loadedTopSongs(result);
+    updateCoverFlow();
+  });
 };
 
 function initialize() {
@@ -21,19 +27,54 @@ function initialize() {
           params[pair[0]] = pair[1];
       }
   }
-  console.dir(params);
   if (params['sf'] != undefined) {
     SF = params['sf'];
   }
-  //document.getElementById('feed_frame').addEventListener('load', loadedTopSongsFromFrame);
+  if (params['l'] != undefined) {
+    L = params['l'];
+  }
+
+  loadStorefrontMappings(function(result){
+    $('select#storefront option[id!=""]').remove();
+    for (key in result) {
+      var option = $("<option></option>").attr({
+        "id": result[key].storefrontId,
+        "value": result[key].storefrontId,
+        "name": result[key].code
+      }).text(result[key].name);
+      $("select#storefront").append(option);
+    }
+  });
+
+  $("select#storefront").on('change', storefrontChanged);
+}
+
+function storefrontChanged(event) {
+  SF = event.target.value;
+  L = $("select#storefront option:selected").attr("name").toLowerCase();
+  loadTopSongs(result => {
+    loadedTopSongs(result);
+    updateCoverFlow();
+  });
+}
+
+function loadStorefrontMappings(callback) {
+  jQuery.ajax({
+    crossDomain:true,
+    dataType:'json',
+    isLocal:true,
+    url:STOREFRONT_MAPPING_PATH,
+    method: "GET"
+  }).then(result => {
+    if (callback != undefined) {
+      callback(result);
+    }
+  });
 }
 
 function loadTopSongs(callback) {
-  // const request = new XMLHttpRequest();
-  // request.addEventListener("load", callback, false);
-  // let url = TOPSONGS_URL;
-  // request.open("GET", url);
-  // request.send(null);
+  $("#loading").attr('class', '');
+  $("div.cover").remove();
 
   let url = TOPSONGS_URL;
   if (location.href.indexOf('https://') == 0) {
@@ -42,21 +83,28 @@ function loadTopSongs(callback) {
   if (SF != undefined) {
     url = url.replace("/xml", "/sf=" + SF + "/xml");
   }
-    console.log(url);
+  if (L != undefined) {
+    var oldurl = "";
+    while (url != oldurl) {
+      oldurl = url;
+      url = url.replace("us", L);
+    }
+  }
 
   jQuery.ajax({
     crossDomain:true,
     url:url,
     method: "GET"
   }).then(result => {
-    loadedTopSongs(result);
+    if (callback != undefined) {
+      callback(result);
+    }
   });
 }
 
 function loadedTopSongs(result) {
   $("#loading").attr('class', 'hidden');
   let title = result.getElementsByTagName('title');
-  console.dir(title);
   $('#chart_title').text(title[0].textContent);
   const entries = result.getElementsByTagName('entry');
   for (let i = 0; i < entries.length; i++) {
@@ -72,7 +120,7 @@ function loadedTopSongs(result) {
       }
       if (child.nodeName == 'title') {
         $(imgElem).attr('name', child.textContent);
-        $(titleElem).text(child.textContent);
+        // $(titleElem).text(child.textContent);
       }
       if (child.nodeName == 'im:image') {
         $(imgElem).attr('src', child.textContent);
@@ -83,12 +131,20 @@ function loadedTopSongs(result) {
     $('#result').append(coverElem);
   }
   $('.coverflow').coverflow({
+    index: 0,
     select: function() {
-      $('.coverflow .cover img').reflect();
+      updateCoverFlow();
     },
     change: function() {
-      $('.coverflow .cover img').reflect();
+      updateCoverFlow();
     }
   });
+  $('.coverflow').coverflow('refresh');
+  $('.coverflow .cover img').reflect();
+}
+
+function updateCoverFlow(e) {
+  $("#title_link").text($(".cover.current img").attr("name"));
+  $("#title_link").attr("href", $(".cover.current a").attr("href"));
   $('.coverflow .cover img').reflect();
 }
